3185fcb7892170c03b77ff24b3d51542098c4a42
Sukhendu Sekhar Guria <sukhenduguria2017@gmail.com>
2024-01-05 11:26:58 +0530

    Boilerplate Code form WPPB

---
+++ includes/class-wp-book-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://github.com/Sukhendu2002/
+ * @since      1.0.0
+ *
+ * @package    Wp_Book
+ * @subpackage Wp_Book/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Book
+ * @subpackage Wp_Book/includes
+ * @author     Sukhendu Sekhar Guria <dev.sukhendu@gmail.com>
+ */
+class Wp_Book_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
